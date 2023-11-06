ab41ec41b974a82ec37a7401f3ffae7dbc4e718b
Júlio César Queiroz Carvalho <juliocarvalho5998@gmail.com>
2023-11-06 12:51:05 -0300

    removido logsde debugação

--- frontEnd/services/common/getImageProfile.js
+++ frontEnd/services/common/getImageProfile.js
@@ -15,14 +15,11 @@ export default async( {setImage, _id} ) => {
 
         if (valueInCache !== null && valueInCache.expired >= Date.now()) {
 
-            console.log('peguei do cache');
             setImage(valueInCache.base64Image);
             
         } else {
 
             await AsyncStorage.removeItem(`ProfileImage.${_id}`);
-
-            console.log('n peguei do cache');
 
             const response = await axios.get(`${URL_API_BACKEND}/getProfileImage/${_id }`);
         
@@ -31,16 +28,13 @@ export default async( {setImage, _id} ) => {
 
             setImage(base64Image);
 
-            console.log('armazenando');
-
             const data = {
                 base64Image, 
                 expired: Date.now() + 5 * 10 * 1000,
             };
 
             await AsyncStorage.setItem(`ProfileImage.${_id}`, JSON.stringify(data));
-            console.log('Dados armazenados temporariamente no cache com sucesso.');
-            
+
         }
 
     } catch (error) {

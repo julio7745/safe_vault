6360722ab0ee0f662233750522b804c01af2c6a3
Júlio César Queiroz Carvalho <juliocarvalho5998@gmail.com>
2023-11-06 12:25:49 -0300

    agora imagem de usuario é armazenada no cache para não carregar 100% das vezes diretamente do banco de dados

--- frontEnd/services/common/getImageProfile.js
+++ frontEnd/services/common/getImageProfile.js
@@ -1,8 +1,9 @@
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import { Buffer } from 'buffer';
 global.Buffer = Buffer;
-import base64 from 'base-64';
+
 
 import { URL_API_BACKEND } from 'react-native-dotenv';
 
@@ -10,11 +11,37 @@ export default async( {setImage, _id} ) => {
 
     try {
 
-        const response = await axios.get(`${URL_API_BACKEND}/getProfileImage/${_id }`);
+        const valueInCache = JSON.parse( await AsyncStorage.getItem(`ProfileImage.${_id}`) );
+
+        if (valueInCache !== null && valueInCache.expired >= Date.now()) {
+
+            console.log('peguei do cache');
+            setImage(valueInCache.base64Image);
+            
+        } else {
+
+            await AsyncStorage.removeItem(`ProfileImage.${_id}`);
+
+            console.log('n peguei do cache');
+
+            const response = await axios.get(`${URL_API_BACKEND}/getProfileImage/${_id }`);
         
-        const buffer = Buffer.from(response.data.imageBuffer, 'binary');
-        const base64Image = buffer.toString('base64');
-        setImage(base64Image);
+            const buffer = Buffer.from(response.data.imageBuffer, 'binary');
+            const base64Image = buffer.toString('base64');
+
+            setImage(base64Image);
+
+            console.log('armazenando');
+
+            const data = {
+                base64Image, 
+                expired: Date.now() + 5 * 10 * 1000,
+            };
+
+            await AsyncStorage.setItem(`ProfileImage.${_id}`, JSON.stringify(data));
+            console.log('Dados armazenados temporariamente no cache com sucesso.');
+            
+        }
 
     } catch (error) {
         console.log(error);

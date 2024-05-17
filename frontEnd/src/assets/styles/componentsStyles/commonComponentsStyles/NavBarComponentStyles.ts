1ec48cfde3ce71c508fa2b6c50da0304b16032ba
Júlio César Queiroz Carvalho <juliocarvalho5998@gmail.com>
2024-05-17 16:11:34 -0300

    agora navbar funciona corretamente

--- frontEnd/src/assets/styles/componentsStyles/commonComponentsStyles/NavBarComponentStyles.ts
+++ frontEnd/src/assets/styles/componentsStyles/commonComponentsStyles/NavBarComponentStyles.ts
@@ -1,4 +1,4 @@
 export default {
-  container: "w-full h-20 flex flex-row justify-center itens-center bg-darkBlue absolute bottom-0 py-4 pb-5",
+  container: "w-full h-20 flex flex-row justify-center itens-center bg-darkBlue absolute bottom-0 py-4 pb-5 ",
   verticalLine: "h-full w-10",
 }

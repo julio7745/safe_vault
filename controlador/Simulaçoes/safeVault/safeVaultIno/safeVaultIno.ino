1f999723fe39d1681686c98b8137cfda10f1563b
julio7745 <juliocarvalho5998@gmail.com>
2024-10-20 19:27:46 -0300

    eletronica

--- controlador/Simulaçoes/safeVault/safeVaultIno/safeVaultIno.ino
+++ controlador/Simulaçoes/safeVault/safeVaultIno/safeVaultIno.ino
@@ -1,16 +1,21 @@
 // Constants
 
 // Pins 
-#define pin74HC595Input 2
+
+// Output 
+#define pin74HC595SerialInput 2
 #define pin74HC595Clock 3
 #define pin74HC595Update 4
+
+// Input
+#define pin74HC165Output 5
 
 // Variables
 
 // Output Variables
 
 // 74HC165 Control
-bool var74HC165Input = true;
+bool var74HC165Update = true;
 bool var74HC165Clock = true;
 
 // KeyPad Collums
@@ -27,41 +32,52 @@ bool varLedDigito4 = false;
 // Lock
 bool varLock = false;
 
+// Sensor Control
+bool varSensorOn = false;
+
 // Input Variables
 
 // KeyPad Buttons
-bool varKeyPadButtoms[9] = {false, false, false, false, false, false, false, false, false};
+bool varKeyPadButtoms[12] = {false, false, false, false, false, false, false, false, false, false, false, false};
 
 void setup() {
 
-//  Serial.begin(9600);
-//  delay(100);
-//  Serial.println("iniciado");
+  // Serial Init
+  Serial.begin(9600);
+  Serial.println("Iniciado Terminal");
 
-  pinMode(pin74HC595Input, OUTPUT);
+  // Pins Init
+  
+  // Output 
+  pinMode(pin74HC595SerialInput, OUTPUT);
+  digitalWrite(pin74HC595SerialInput, LOW);
   pinMode(pin74HC595Clock, OUTPUT);
-  pinMode(pin74HC595Update, OUTPUT);
-
-  digitalWrite(pin74HC595Input, LOW);
   digitalWrite(pin74HC595Clock, LOW);
+  pinMode(pin74HC595Update, OUTPUT);
   digitalWrite(pin74HC595Update, LOW);
- 
 
+  // Input
+  pinMode(pin74HC165Output, INPUT);
+  
 }
 
 void updateOutputs () {
 
+  delay(100);
+
   bool outputs[] = {
-    var74HC165Input, var74HC165Clock,
+    var74HC165Update, var74HC165Clock,
     varKeyPadC1, varKeyPadC2,varKeyPadC3, 
     varLedDigito1, varLedDigito2, varLedDigito3, varLedDigito4,
-    varLock
+    varLock,
+    varSensorOn
   };
+  
   int outputsLength = sizeof(outputs) / sizeof(outputs[0]);
 
   for (int i = outputsLength - 1; i >= 0; i--) {
 
-    digitalWrite(pin74HC595Input, outputs[i]);
+    digitalWrite(pin74HC595SerialInput, outputs[i]);
     digitalWrite(pin74HC595Clock, HIGH);
     digitalWrite(pin74HC595Clock, LOW);
     
@@ -76,6 +92,7 @@ void updateInputs() {
     if (Serial.available()) {
         // Cria um buffer para armazenar a linha lida
         String input = Serial.readStringUntil('\n'); // Lê até a nova linha
+        Serial.println(input);
         // Localiza o sinal de igual
         int equalsIndex = input.indexOf('=');
         
@@ -90,37 +107,7 @@ void updateInputs() {
             varValue.trim();
 
             // Verifica e atribui o valor correspondente à variável
-            if (varName == "var74HC165Input") {
-                if (varValue == "LOW") {
-                    var74HC165Input = LOW;
-                } else if (varValue == "HIGH") {
-                    var74HC165Input = HIGH;
-                }
-            } else if (varName == "var74HC165Clock") {
-                if (varValue == "LOW") {
-                    var74HC165Clock = LOW;
-                } else if (varValue == "HIGH") {
-                    var74HC165Clock = HIGH;
-                }
-            }if (varName == "varKeyPadC1") {
-                if (varValue == "LOW") {
-                    varKeyPadC1 = LOW;
-                } else if (varValue == "HIGH") {
-                    varKeyPadC1 = HIGH;
-                }
-            } else if (varName == "varKeyPadC2") {
-                if (varValue == "LOW") {
-                    varKeyPadC2 = LOW;
-                } else if (varValue == "HIGH") {
-                    varKeyPadC2 = HIGH;
-                }
-            } else if (varName == "varKeyPadC3") {
-                if (varValue == "LOW") {
-                    varKeyPadC3 = LOW;
-                } else if (varValue == "HIGH") {
-                    varKeyPadC3 = HIGH;
-                }
-            } else if (varName == "varLedDigito1") {
+            if (varName == "varLedDigito1") {
                 if (varValue == "LOW") {
                     varLedDigito1 = LOW;
                 } else if (varValue == "HIGH") {
@@ -133,7 +120,6 @@ void updateInputs() {
                     varLedDigito2 = HIGH;
                 }
             } else
-
              if (varName == "varLedDigito3") {
                 if (varValue == "LOW") {
                     varLedDigito3 = LOW;
@@ -152,13 +138,131 @@ void updateInputs() {
                 } else if (varValue == "HIGH") {
                     varLock = HIGH;
                 }
+            } else if (varName == "varSensorOn") {
+                if (varValue == "LOW") {
+                    varSensorOn = LOW;
+                } else if (varValue == "HIGH") {
+                    varSensorOn = HIGH;
+                }
             } 
         }
     }
 }
 
 void updateKeyPad() {
-    // Função de atualização do teclado (a ser implementada)
+
+  bool varDigito1 = false;
+  bool varDigito2 = false;
+  bool varDigito3 = false;
+  bool varDigito4 = false;
+  bool varDigito5 = false;
+  bool varDigito6 = false;
+  bool varDigito7 = false;
+  bool varDigito8 = false;
+  bool varDigito9 = false;
+  bool varDigitoA = false;
+  bool varDigito0 = false;
+  bool varDigitoB = false;
+
+  varKeyPadC1 = HIGH;
+  varKeyPadC2 = LOW;
+  varKeyPadC3 = LOW;
+  updateOutputs();
+
+  var74HC165Update = LOW;
+  updateOutputs();
+  var74HC165Update = HIGH;
+  updateOutputs();
+
+  varDigito1 = digitalRead(pin74HC165Output);
+
+  var74HC165Clock = LOW;
+  updateOutputs();
+  var74HC165Clock = HIGH;
+  updateOutputs();
+
+  varDigito4 = digitalRead(pin74HC165Output);
+
+  var74HC165Clock = LOW;
+  updateOutputs();
+  var74HC165Clock = HIGH;
+  updateOutputs();
+
+  varDigito7 = digitalRead(pin74HC165Output);
+
+  var74HC165Clock = LOW;
+  updateOutputs();
+  var74HC165Clock = HIGH;
+  updateOutputs();
+
+  varDigitoA = digitalRead(pin74HC165Output);
+
+  varKeyPadC1 = LOW;
+  varKeyPadC2 = HIGH;
+  varKeyPadC3 = LOW;
+  updateOutputs();
+
+  var74HC165Update = LOW;
+  updateOutputs();
+  var74HC165Update = HIGH;
+  updateOutputs();
+
+  varDigito2 = digitalRead(pin74HC165Output);
+
+  var74HC165Clock = LOW;
+  updateOutputs();
+  var74HC165Clock = HIGH;
+  updateOutputs();
+
+  varDigito5 = digitalRead(pin74HC165Output);
+
+  var74HC165Clock = LOW;
+  updateOutputs();
+  var74HC165Clock = HIGH;
+  updateOutputs();
+
+  varDigito8 = digitalRead(pin74HC165Output);
+
+  var74HC165Clock = LOW;
+  updateOutputs();
+  var74HC165Clock = HIGH;
+  updateOutputs();
+
+  varDigito0 = digitalRead(pin74HC165Output);
+
+  varKeyPadC1 = LOW;
+  varKeyPadC2 = LOW;
+  varKeyPadC3 = HIGH;
+  updateOutputs();
+
+  var74HC165Update = LOW;
+  updateOutputs();
+  var74HC165Update = HIGH;
+  updateOutputs();
+
+  varDigito3 = digitalRead(pin74HC165Output);
+
+  var74HC165Clock = LOW;
+  updateOutputs();
+  var74HC165Clock = HIGH;
+  updateOutputs();
+
+  varDigito6 = digitalRead(pin74HC165Output);
+
+  var74HC165Clock = LOW;
+  updateOutputs();
+  var74HC165Clock = HIGH;
+  updateOutputs();
+
+  varDigito9 = digitalRead(pin74HC165Output);
+
+  var74HC165Clock = LOW;
+  updateOutputs();
+  var74HC165Clock = HIGH;
+  updateOutputs();
+
+  varDigitoB = digitalRead(pin74HC165Output);
+  
 }
 
 void loop() {
